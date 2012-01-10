24f53223fdbd7448ad15f1e025b96696c8289416
Benjamin Roux <stuart.benji@gmail.com>
2012-01-09 23:50:11 -0500

    Modified opendot script to make the outputed to image clearer

--- opendot.sh
+++ opendot.sh
@@ -4,6 +4,6 @@ FILE=$(echo $1 | sed 's/\(.*\)\..*/\1/')
 echo "Creating "$FILE".png"
 FILE=$FILE".png"
 touch $FILE
-dot -Tpng -o$FILE $1
+dot -Gsize="10,10" -Gratio=fill -Gsplines=spline -Tpng -o$FILE $1
 echo "Opening "$FILE
 gnome-open $FILE &
