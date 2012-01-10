707bf3d2c8910c40aede6e1259fc57511854b0ea
Benjamin Roux <stuart.benji@gmail.com>
2012-01-10 00:07:00 -0500

    Updated main function used for testing to try interpolating with the ANN

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,13 @@
+/* This file is for testing purposes only */
+
 #include "network.hh"
 #include "exceptions.hh"
 #include "parser.hh"
 #include <fstream>
 #include <string>
 #include <stack>
-
+#define NB_OUTPUTS 2
+#define NB_INPUTS 3
 
 int main (int argc, char** argv)
 {
@@ -25,6 +28,27 @@ int main (int argc, char** argv)
     fs << "{" << std::endl;
     network->dotify (fs);
     fs << "}" << std::endl;
+
+    double outputs[NB_OUTPUTS];
+    double inputs[NB_INPUTS];
+
+    for (unsigned ipt = 0; ipt < NB_INPUTS; ipt++)
+    {
+      std::cout << "Enter input no " << ipt << std::endl;
+      std::cin >> inputs[ipt];
+    }
+    network->interpolate(outputs, inputs);
+
+    std::cout << "inputs : " << std::endl;
+    for (int i = 0; i < NB_INPUTS - 1; i++)
+      std::cout << inputs[i] << ", ";
+    std::cout << inputs[NB_INPUTS - 1] << std::endl;
+
+    std::cout << "outputs : " << std::endl;
+    for (int i = 0; i < NB_OUTPUTS - 1; i++)
+      std::cout << outputs[i] << ", ";
+    std::cout << outputs[NB_OUTPUTS - 1] << std::endl;
+
   }
   catch (NoPerceptronException ex)
   {
