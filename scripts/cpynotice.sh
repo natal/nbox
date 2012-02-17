2bebd540e4aaba84e84ddbd4fbc10024eee54960
Roux Benjamin <roux_b@epita.fr>
2012-02-17 19:02:20 +0100

    Officialy moved the FANN project under the GPL GNU public licence

---
+++ scripts/cpynotice.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+HEAD='/* Interface code for dealing with text properties.
+   Copyright (C) 1993, 1994, 1995, 1997, 1999, 2000, 2001
+   Free Software Foundation, Inc.
+
+   This file is part of FANN.
+
+   FANN is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   FANN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with FANN; see the file COPYING.
+   If not, see <http://www.gnu.org/licenses/>.  */'
+
+
+for f in $@; do
+   FILE_SAVE=$f'_sv'
+   cat $f > $FILE_SAVE
+   echo "$HEAD" > $f
+   echo '' >> $f
+   echo '' >> $f
+   cat $FILE_SAVE >> $f
+   rm $FILE_SAVE
+done
